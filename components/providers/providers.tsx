7acc616e6fd4e63ef7d89ee86a8c065cf33ed718
envyvox <envyvox@gmail.com>
2024-06-10 05:56:31 +0300

    chore(react-query): added react-query

--- components/providers/providers.tsx
+++ components/providers/providers.tsx
@@ -1,14 +1,17 @@
+import ReactQueryProvider from "./react-query-provider";
 import { ThemeProvider } from "./theme-provider";
 
 export const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
-    <ThemeProvider
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-    >
-      {children}
-    </ThemeProvider>
+    <ReactQueryProvider>
+      <ThemeProvider
+        attribute="class"
+        defaultTheme="system"
+        enableSystem
+        disableTransitionOnChange
+      >
+        {children}
+      </ThemeProvider>
+    </ReactQueryProvider>
   );
 };
---
+++ components/providers/react-query-provider.tsx
@@ -0,0 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const queryClient = new QueryClient();
+
+const ReactQueryProvider = ({ children }: Props) => {
+  return (
+    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+  );
+};
+
+export default ReactQueryProvider;
